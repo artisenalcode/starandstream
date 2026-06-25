7d16679b5a77a5bc2d94afe744a2b60897718c7d
Al Tolentino <codegeek@outlook.com>
2026-06-25 14:25:01 +1000

    refactor: migrate starandstream.com to fully static SSG hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,13 +5,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://starandstream.com",
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    clientAddressResolution: "x-forwarded-for",
-  },
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
